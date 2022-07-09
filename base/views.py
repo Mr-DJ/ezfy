79fc14697e1f58b2e15f3ae7b4778b4b77406492
Samuel Jonathan <jonathandinesh@gmail.com>
2022-07-09 16:08:40 +0530

    configure yootoob route

--- base/views.py
+++ base/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+# from utils import everything
 # Create your views here.
 def home(request):
     return render(request, 'base/home.html')
+
+def youtoob(request):
+    print("this is a simple response")
+    print(request.GET['ytInput'])
+    # return HttpResponse("""everything.printURL(request.GET['ytInput'])
+    return render(request, 'base/home.html', {'ytOutput':request.GET['ytInput']})
