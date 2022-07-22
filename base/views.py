77f8ad2bfc399a28e8d3e56ec5e861f07d9849a9
Samuel Jonathan <jonathandinesh@gmail.com>
2022-07-22 13:54:47 +0530

    oops

--- base/views.py
+++ base/views.py
@@ -17,8 +17,9 @@ def convert(request):
     # request.session['convertURI'] = request.GET['ytInput']
     migrate = controller.migrate()
     result = migrate.start_conversion(request.GET['ytInput'])
-    result_dict = json.loads(result)
-    return render(request, 'base/home.html', {'ytOutput': result_dict, 'loggedIn': controller.logState(request.session['loggedIn']), 'c_type': migrate.c_type})
+    if(migrate.c_type == 'sp2yt'):
+        result = json.loads(result)
+    return render(request, 'base/home.html', {'ytOutput': result, 'loggedIn': controller.logState(request.session['loggedIn']), 'c_type': migrate.c_type})
 
 def login(request): 
     controller.login()
